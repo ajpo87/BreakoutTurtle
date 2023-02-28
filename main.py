b0c0949ed40ab3caf3fe7ae870edff4a3da280d2
ajpo87 <antonio.oliveira87@gmail.com>
2023-02-28 22:43:23 +0000

    Dectect colisions and ball movement

--- main.py
+++ main.py
@@ -62,8 +62,45 @@ for i in range(5):
         brick_row.append(brick)
     bricks.append(brick_row)
 
+
+# Detect collisions
+def ball_paddle_collision():
+    if (ball.ycor() < -240 and ball.ycor() > -250) and (
+            ball.xcor() > paddle.xcor() - 50 and ball.xcor() < paddle.xcor() + 50):
+        return True
+    else:
+        return False
+
+
+def ball_brick_collision():
+    for brick_row in bricks:
+        for brick in brick_row:
+            if (ball.ycor() > brick.ycor() - 20 and ball.ycor() < brick.ycor() + 20) and (
+                    ball.xcor() > brick.xcor() - 50 and ball.xcor() < brick.xcor() + 50):
+                brick.hideturtle()
+                brick_row.remove(brick)
+                return True
+    return False
+
+
+def ball_wall_collision():
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.dy *= -1
+    if ball.xcor() > 290 or ball.xcor() < -290:
+        ball.dx *= -1
+
+
 while True:
     screen.update()
     time.sleep(0.01)
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    # Detect collisions
+    if ball_paddle_collision():
+        ball.dy *= -1
+    if ball_brick_collision():
+        ball.dy *= -1
+    ball_wall_collision()
 
 turtle.done()
